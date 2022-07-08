81721c6a5e6ebe57653bcc31805c644fdf9ceb27
Ruben Rienstra <rotezdesign@gmail.com>
2022-07-08 14:30:48 +0200

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $posts = Post::all();
+
+    ddd($posts[1]->getContents());
+
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
 });
+
+Route::get('posts/{post}', function ($slug) {
+    return view('post', [
+        'post' => Post::find($slug)
+    ]);
+})->where('post', '[A-z_\-]+');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
